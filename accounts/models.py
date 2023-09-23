1c8f9e6b8d3fc632276b7072e6cec3ee3976e92d
maka7172 <kholghahmadi62@gmail.com>
2023-09-23 23:28:45 +0330

    comments accounting urls

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class CustomUser(AbstractUser) :
     age = models.PositiveIntegerField(null=True,blank=True)
+
+
+    
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import CustomUser
+from .forms import CustomUserCrationForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+# class SignUpView(generic.CreateView) :
+#     form_class = CustomUserCrationForm 
+#     template_name = 'signup.html'
+#     success_url = reverse_lazy('login')
+
+
