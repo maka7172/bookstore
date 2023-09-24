42ddaeecd8f7854ac1a52c219eb41ee23b2d2df8
maka7172 <kholghahmadi62@gmail.com>
2023-09-24 14:19:11 +0330

    in accounts add template test

--- accounts/tests.py
+++ accounts/tests.py
@@ -12,4 +12,8 @@ class SignupPageTesting(TestCase) :
     def test_signup_url (self) :
         response = self.client.get('/accounts/signup/')
         self.assertEqual(response.status_code,200) 
+
+    def test_signup_templates(self) :
+        response = self.client.get(reverse('signup'))
+        self.assertTemplateUsed(response,'registration/signup.html')
     
