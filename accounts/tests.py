d22ee80af902bfac52c6fb5f489e27e4ded8b47b
maka7172 <kholghahmadi62@gmail.com>
2023-09-24 15:19:18 +0330

    in accounts add test object

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class SignupPageTesting(TestCase) :
+    username = 'new_user'
+    email = 'new_user@gmail.com'
 
 
     def test_signup_url_by_name (self) :
@@ -16,4 +19,12 @@ class SignupPageTesting(TestCase) :
     def test_signup_templates(self) :
         response = self.client.get(reverse('signup'))
         self.assertTemplateUsed(response,'registration/signup.html')
+
+    def test_signup_redirect(self) :
+        User = get_user_model().objects.create_user(self.username,self.email)
+            
+        
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username,self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email,self.email)
     
