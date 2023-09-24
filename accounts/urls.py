1ac44c7e52dce779606a2ccf877432caaefee7ba
maka7172 <kholghahmadi62@gmail.com>
2023-09-24 12:00:58 +0330

    add home and make login logout signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/',views.SignUpView.as_view(),name='signup_page')
+    path('signup/',views.SignUpView.as_view(),name='signup')
 ]
 
